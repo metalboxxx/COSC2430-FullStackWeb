7a1673f4d00dd9bb4eac7076e9d231ef47754777
unknown <minhkhang09122003@gmail.com>
2022-09-06 16:27:20 +0700

    Refactor files into the www directory

--- www/register.js
+++ www/register.js
@@ -1,21 +1,93 @@
 
-function validatePassword(){
-    let password = document.getElementById("password").value;
-    let confirm_password = document.getElementById("repass").value;
-    let message = document.getElementById("message");
 
-    console.log(password, confirm_password);
-    if(password.length != 0){
-        if(password == confirm_password){
-            message.textContent = "Password match";
-            message.style.backgroundColor = "#3ae374";
-        } else{
-            message.textContent = "Password do not match";
-            message.style.backgroundColor = "#ff4d4d";
-        }
+function FormValidate(){
+    var username = document.getElementById('username').value;
+    var errorUserName = document.getElementById('errorUserName');
+    var regexName = /^[a-zA-Z0-9]{8,15}$/;
+
+    var name = document.getElementById('name').value;
+    var errorName = document.getElementById('errorName');
+
+    var password = document.getElementById('password').value;
+    var errorPassword = document.getElementById('errorPassword');
+    var regexPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}/;
+
+    var address = document.getElementById('address').value;
+    var errorAddress = document.getElementById('errorAddress');
+
+    var email = document.getElementById('email').value;
+    var errorEmail = document.getElementById('errorEmail');
+    var reGexChar = /^.{5,}$/;
+
+
+ 
+    
+    if (username == '' || username == null) {
+        errorUserName.innerHTML = "Please fill in this field!";
+        return false;
+
+    }else if(!regexName.test(username)){
+        errorUserName.innerHTML = "Your username must contain only letters (lower and upper case) and digits, has a length from 8 to 15 characters";
+        return false;
+    }else{
+        errorUserName.innerHTML = '';
     }
-    else{
-        alert("Empty password!");
-        message.textContent="";
+    
+    if (password == '' || password == null) {
+        errorPassword.innerHTML = "Empty Password!";
+        return false;
+
+    }else if(!regexPassword.test(password)){
+        errorPassword.innerHTML = "Password must be from 8 to 20 characters. Each password must contain at least 1 lower case letter, at least 1 upper case letter, at least 1 digit!";
+        return false;
+    }else{
+        errorPassword.innerHTML = '';
     }
+
+    if (name == '' || name == null) {
+        errorName.innerHTML = "Empty Name!";
+        return false;
+
+    }else if(!reGexChar.test(name)){
+        errorName.innerHTML = "Your name must be more than 5 characters";
+        return false;
+    }else{
+        errorName.innerHTML = '';
+    }
+
+
+
+    if (email == '' || email == null) {
+        errorEmail.innerHTML = "Empty Email!";
+        return false;
+
+    }else if(!reGexChar.test(email)){
+        errorEmail.innerHTML = "Please type the correct form of email!";
+        return false;
+    }else{
+        errorEmail.innerHTML = '';
+    }
+
+    if (address == '' || address == null) {
+        errorAddress.innerHTML = "Empty Address!";
+        return false;
+    }else if(!reGexChar.test(address)){
+        errorAddress.innerHTML = "5 or more characters required!";
+        return false;
+    }else{
+        errorAddress.innerHTML = '';
+    }
+
+
+    if (username && name && email && password) {
+        // Người dùng đã nhập đúng thông tin
+        alert("Created account successfully!");
+        window.location.href= "CustomerReg.php";
+
+        return true;
+    }else{
+
+    }
+
+    return false;
 }
